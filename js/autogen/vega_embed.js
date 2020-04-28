ca17410bc75b5a73541a78605d3470fc9a590679
Stephen Macke <smacke@illinois.edu>
2020-04-28 07:45:16 -0700

    deploy

--- js/autogen/vega_embed.js
+++ js/autogen/vega_embed.js
@@ -2,14 +2,18 @@
 function startVegaEmbedding() {
   var embedOpt = {"mode": "vega-lite"};
   $(document).ready(function() {
-    vegaEmbed("#jhu_world_cases", jhu_world_cases, embedOpt);
-    vegaEmbed("#jhu_world_deaths", jhu_world_deaths, embedOpt);
-    vegaEmbed("#jhu_us_cases", jhu_us_cases, embedOpt);
-    vegaEmbed("#jhu_us_deaths", jhu_us_deaths, embedOpt);
-    vegaEmbed("#jhu_world_cases_mobile", jhu_world_cases_mobile, embedOpt);
-    vegaEmbed("#jhu_world_deaths_mobile", jhu_world_deaths_mobile, embedOpt);
-    vegaEmbed("#jhu_us_cases_mobile", jhu_us_cases_mobile, embedOpt);
-    vegaEmbed("#jhu_us_deaths_mobile", jhu_us_deaths_mobile, embedOpt);
+vegaEmbed("#jhu_world_cases", jhu_world_cases, embedOpt);
+vegaEmbed("#jhu_world_deaths", jhu_world_deaths, embedOpt);
+vegaEmbed("#jhu_us_cases", jhu_us_cases, embedOpt).then(function(chart) {
+    chart.view.addSignalListener('click', makePopulateInfoPageSpaceHandler('jhu_us_cases'));
+});
+vegaEmbed("#jhu_us_deaths", jhu_us_deaths, embedOpt).then(function(chart) {
+    chart.view.addSignalListener('click', makePopulateInfoPageSpaceHandler('jhu_us_deaths'));
+});
+vegaEmbed("#jhu_world_cases_mobile", jhu_world_cases_mobile, embedOpt);
+vegaEmbed("#jhu_world_deaths_mobile", jhu_world_deaths_mobile, embedOpt);
+vegaEmbed("#jhu_us_cases_mobile", jhu_us_cases_mobile, embedOpt);
+vegaEmbed("#jhu_us_deaths_mobile", jhu_us_deaths_mobile, embedOpt);
   });
 };
     
